00304ddda952c9ebb92b276a97d356e2843c3edf
benetsugarboy <benet1one@gmail.com>
2025-08-06 15:51:10 +0200

    Attempted to export stan data (and failed)

---
+++ data-raw/cmdstan_fit.R
@@ -0,0 +1,10 @@
+# model <- cmdstan_model("data-raw/model.stan")
+# cmdstan_fit <- run_cmdstan(
+#     model = model,
+#     data = list(y = 3),
+#     chains = 4,
+#     iter = 100,
+#     seed = 123
+# )
+#
+# usethis::use_data(cmdstan_fit, overwrite = TRUE)
---
+++ data-raw/stan_fit.R
@@ -0,0 +1,9 @@
+# model <- stan_model("data-raw/model.stan")
+# stan_fit <- run_stan(
+#     model = model,
+#     data = list(y = 3),
+#     chains = 4,
+#     iter = 100
+# )
+#
+# usethis::use_data(stan_fit, overwrite = TRUE)
