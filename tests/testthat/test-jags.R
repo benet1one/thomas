6050ed03a871cb2252ceebc9f557db6c653649cd
benetsugarboy <benet1one@gmail.com>
2025-11-15 13:24:47 +0100

    tests for draw extraction

--- tests/testthat/test-jags.R
+++ tests/testthat/test-jags.R
@@ -1,4 +1,6 @@
 
+# setwd("tests/testthat")
+
 my_model <- jags_model({
     a ~ dexp(0.01)
     b ~ dexp(0.01)
@@ -53,8 +55,13 @@ test_that("file and model", {
 })
 
 
+draws <- get_draws(jags_fit, as = "df")
+draws[[1, ]]
 
+draws_listed <- get_draws(jags_fit, as = "df_listed")
+draws_listed[[1, ]]
 
-
-
-
+test_that("exactly_one_draw", {
+    expect_error( draws[[1:2, ]] )
+    expect_error( draws[[integer(), ]] )
+})
