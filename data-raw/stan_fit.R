310bb9e8ed00817f8f18ff3ba95140b415de358a
benetsugarboy <benet1one@gmail.com>
2025-11-21 23:38:19 +0100

    Removed unused files

--- data-raw/stan_fit.R
+++
@@ -1,9 +0,0 @@
-# model <- stan_model("data-raw/model.stan")
-# stan_fit <- run_stan(
-#     model = model,
-#     data = list(y = 3),
-#     chains = 4,
-#     iter = 100
-# )
-#
-# usethis::use_data(stan_fit, overwrite = TRUE)
